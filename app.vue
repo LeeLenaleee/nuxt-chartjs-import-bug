8ea50c1163d7b0d10366a5e6eba5fa36d9dce7cf
Jacco van den Berg <jaccoberg2281@gmail.com>
2022-10-12 20:23:35 +0200

    sample setup

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Test123</h1>
+  <canvas id="ff"></canvas>
+  <h2>Test321</h2>
+</template>
+
+<script setup lang="ts">
+import { Chart, registerables } from "chart.js";
+
+
+  Chart.register(...registerables);
+  new Chart("ff", {
+    type: "doughnut",
+    data: {
+      labels: ["chart", "js"],
+      datasets: [
+        {
+          data: [2, 5],
+        },
+      ],
+    },
+  });
+</script>
